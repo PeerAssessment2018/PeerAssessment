05e172f8fa865d32dc715d294df314b8a0b64bcd
indranilbiswas7 <indranilbiswas7@gmail.com>
2018-06-12 16:10:36 +0530

    mec_2

--- JavaApplication1/src/machanism.java
+++ JavaApplication1/src/machanism.java
@@ -14,14 +14,15 @@ import java.util.*;
 public class machanism 
 {
     int number_assessor;
-    public void distribute_answers(String course_id,String question_id)
+    public int[][] distribute_answers(String course_id,String question_id)
     {
+        int arr[][]=null;
         try {
             Connection conn=null;
             Properties conn_prop=new Properties();
             conn_prop.put("user", "root");
             conn_prop.put("password", "");
-            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test",conn_prop);
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/peer_assesment",conn_prop);
             
             String sql = "SELECT no_assessor from peer_assessment course_id = '"+course_id+"'";
             Statement stmt=conn.createStatement();
@@ -35,7 +36,7 @@ public class machanism
                    dh.conn.close();
                    ArrayList answers2=(ArrayList)answers.clone();
                    int len=answers.size();
-                   int arr[][]=new int[len*number_assessor][2];
+                    arr=new int[len*number_assessor][2];
                   
                    int n=number_assessor;
                    
@@ -47,36 +48,47 @@ public class machanism
                           Random random = new Random();
                           int index = random.nextInt(len);
                           int x=(int)answers.get(index);
-                          while(x!=arr[i+n-1][0])
+                          while(x==arr[i+n-1][0])
                           {
                               random = new Random();
                               index = random.nextInt(len);
                               x=(int)answers.get(index);
                           }
-                            arr[i+n-1][1]=x;
-                              
+                          arr[i+n-1][1]=x;       
                       }
                    }
-            /*for(int i=0;i<len;i++)
-            {
-             while(n)
-                {
-                  Random random = new Random();
-                 int index = random.nextInt(len);
-                 
-                 //answers.remove(index);
-                 arr[i+(l-n)][0]=(int)
-                 arr[i+(l-n)][1]=(int)((ArrayList)answers.get(index)).get(0);
-                 n--;
-                 //return as an 2d array where the assignment takes place 
-                }
-               // answers=answers2;
-                n=number_assessor;
-            }*/
             
         } catch (Exception e) 
         {
             System.out.println("machanism.distribute_answers() "+e.getMessage());
         }
+        return arr;
+    }
+    
+    public int[] get_allocated(int id,int couse_id)
+    {
+        int arr[]=null;
+        try {
+            Connection conn=null;
+            Properties conn_prop=new Properties();
+            conn_prop.put("user", "root");
+            conn_prop.put("password", "");
+            conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/peer_assesment",conn_prop);
+            
+            String sql = "SELECT user_id from pa_grade course_id = '"+id+"'";
+            Statement stmt=conn.createStatement();
+            ResultSet rs=stmt.executeQuery(sql);
+            ArrayList arrL=new ArrayList();
+            while(rs.next())
+            {
+                arrL.add(rs.getString("user_id"));
+            }
+            conn.close();
+            
+            //for()
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return arr;
     }
 }
